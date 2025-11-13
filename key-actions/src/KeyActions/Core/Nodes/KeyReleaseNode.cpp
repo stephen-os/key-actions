cd4b6e957bfab8686020895c1a56ba4cb30ea476
stephen-os <ImStephen.1sw@gmail.com>
2025-11-12 19:44:05 -0500

    Key release node.

---
+++ key-actions/src/KeyActions/Core/Nodes/KeyReleaseNode.cpp
@@ -0,0 +1,51 @@
+#include "KeyReleaseNode.h"
+
+#include <iostream>
+
+#include "Lumina/Core/Log.h"
+#include "Lumina/Core/Assert.h"
+#include "Lumina/Core/Input.h"
+
+namespace KeyActions
+{
+    Lumina::Ref<KeyReleaseNode> KeyReleaseNode::Create(Lumina::KeyCode key)
+    {
+        return Lumina::CreateRef<KeyReleaseNode>(key);
+    }
+
+    KeyReleaseNode::KeyReleaseNode(Lumina::KeyCode key) : Node("KeyPress"), m_Key(key)
+    {
+        m_Name = "Press " + Lumina::Input::KeyCodeToString(m_Key);
+        AddPin(CreatePin("Input", PinType::Input));
+        AddPin(CreatePin("Output", PinType::Output));
+    }
+
+    Node::NodePtr KeyReleaseNode::Execute(Lumina::GlobalInputPlayback* playback)
+    {
+        LUMINA_ASSERT(m_Key != Lumina::KeyCode::Unknown, "KeyPressNode: Invalid key code");
+        LUMINA_ASSERT(playback != nullptr, "KeyPressNode: Playback system is null in KeyPressNode execution");
+
+        playback->SimulateKeyPress(m_Key);
+        LUMINA_LOG_INFO("Simulated key press of {}", Lumina::Input::KeyCodeToString(m_Key));
+
+        Pin* outputPin = GetPin(PinType::Output);
+        if (outputPin && outputPin->ConnectedNode)
+            return outputPin->ConnectedNode;
+
+        return nullptr;
+    }
+
+    bool KeyReleaseNode::CanConnect(PinType sourceType, PinType targetType)
+    {
+        // Rule: KeyPressNode allows bidirectional connections
+        // 1. Output pins can connect to Input pins
+        // 2. Input pins can connect to Output pins
+        return ((sourceType == PinType::Output && targetType == PinType::Input) ||
+            (sourceType == PinType::Input && targetType == PinType::Output));
+    }
+
+    Lumina::KeyCode KeyReleaseNode::GetKey() const
+    {
+        return m_Key;
+    }
+}
---
+++ key-actions/src/KeyActions/Core/Nodes/KeyReleaseNode.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Node.h"
+#include "Lumina/Core/KeyCodes.h"
+
+namespace KeyActions
+{
+    class KeyReleaseNode : public Node
+    {
+    public:
+        static Lumina::Ref<KeyReleaseNode> Create(Lumina::KeyCode key);
+
+        KeyReleaseNode(Lumina::KeyCode key);
+
+        NodePtr Execute(Lumina::GlobalInputPlayback* playback) override;
+
+        NodeType GetType() const override { return NodeType::KeyPress; }
+
+        bool CanConnect(PinType sourceType, PinType targetType) override;
+
+        Lumina::KeyCode GetKey() const;
+
+    private:
+        Lumina::KeyCode m_Key;
+    };
+}
