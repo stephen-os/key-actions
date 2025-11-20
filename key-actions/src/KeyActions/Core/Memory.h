84c31fdda839e82a05299eb3ad8777b62691a1ea
stephen-os <ImStephen.1sw@gmail.com>
2025-11-19 20:53:17 -0500

    Added CreateRef wrapper to our Memory.h

--- key-actions/src/KeyActions/Core/Memory.h
+++ key-actions/src/KeyActions/Core/Memory.h
@@ -6,4 +6,9 @@ namespace KeyActions
 {
     template<typename T>
     using Ref = Lumina::Ref<T>;
+    template<typename T, typename... Args>
+    inline Ref<T> CreateRef(Args&&... args)
+    {
+        return Lumina::CreateRef<T>(std::forward<Args>(args)...);
+    }
 }
